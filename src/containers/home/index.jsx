a74d1644e593b69794803e57f366095ac21afd8b
Lyght <valentinpeltier@yahoo.fr>
2019-01-11 01:06:14 +0100

    Edit style and add home content

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,8 +1,101 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import partners from '../../variables/partners'
+
+import posterImage from '../../assets/poster.png';
+
+import 'owl.carousel2/dist/assets/owl.carousel.css';
+import './home.css';
+
+const $ = require('jquery');
+window.jQuery = $;
+require('owl.carousel2');
 
 class Home extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.partners = [];
+
+		partners.forEach((partner, i) => {
+			const img = require(`../../assets/partners/${partner.image}`);
+
+			this.partners.push(<img src={img} alt={partner.name} key={i} />);
+		});
+	}
+
+	componentDidMount() {
+		$('.owl-carousel').owlCarousel({
+			responsive: {
+        0: {
+					items: 2
+        },
+        400: {
+					items: 3
+				},
+				550: {
+					items: 4
+				},
+				800: {
+					items: 5
+				},
+        1000: {
+					items: 6
+        }
+    	},
+			loop: true,
+			margin: 10,
+			autoplay: true,
+			autoplayTimeout: 1500,
+			smartSpeed: 500,
+			autoplaySpeed: 1000
+		});
+	}
+
   render() {
-    return 'Accueil';
+    return (
+			<div id="home">
+				<h1>Bienvenue sur le site officiel du Gala UTT !</h1>
+				<hr />
+
+				<p className="justified">
+					Le Gala de l’UTT est un soirée de prestige unique en son genre accueillant 3500 participants
+					et dont les bénéfices sont reversés à des associations de charité.
+					Nous revenons en 2019 pour une 23ème édition.
+					Les locaux de l’UTT seront de nouveau transformés afin de vous faire vivre une nuit magique.
+				</p>
+
+				<div className="centered">
+					<div className="poster-container">
+						<img src={posterImage} alt="" />
+					</div>
+
+					<hr />
+
+					<h2>Aftermovie Gala UTT 2018</h2>
+					<iframe
+						title="Aftermovie Gala UTT 2018"
+						src="https://www.youtube.com/embed/EO_rrd8FfSM"
+						width="720"
+						height="405"
+						frameBorder="0"
+						allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+						allowFullScreen
+					></iframe>
+
+					<hr />
+
+					<h2>Partenaires</h2>
+
+					<div className="owl-carousel">
+						{ this.partners }
+					</div>
+
+					<Link to="/partenaires" className="button">Tous les partenaires</Link>
+				</div>
+			</div>
+		);
   }
 };
 
---
+++ src/variables/partners.js
@@ -0,0 +1,107 @@
+export default [
+	{
+		name: 'Allyson',
+		image: 'allyson.png',
+		url: 'http://ae-allyson.com/'
+	},
+	{
+		name: 'Amnesty International',
+		image: 'amnesty-international.png',
+		url: 'https://www.amnesty.org/fr/'
+	},
+	{
+		name: 'BDE UTT',
+		image: 'bde-utt.png',
+		url: 'https://etu.utt.fr/orgas/bde'
+	},
+	{
+		name: 'Campus 3',
+		image: 'campus3.png',
+		url: 'https://campus3.fr/'
+	},
+	{
+		name: 'Fengee',
+		image: 'fengee.png',
+		url: 'https://www.fengee.fr/'
+	},
+	{
+		name: 'Festilight',
+		image: 'festilight.png',
+		url: 'https://www.festilight.com/'
+	},
+	{
+		name: 'Gilbert Lemelle',
+		image: 'gilbert-lemelle.png',
+		url: 'https://gilbert-lemelle.fr/'
+	},
+	{
+		name: 'GUINTOLI',
+		image: 'guintoli.png',
+		url: 'http://www.groupe-nge.fr/filiales/guintoli'
+	},
+	{
+		name: 'ID Verde',
+		image: 'idverde.png',
+		url: 'https://idverde.com/'
+	},
+	{
+		name: 'K2A',
+		image: 'k2a.png',
+		url: 'https://www.k2a-club.com/'
+	},
+	{
+		name: 'Loxam Access',
+		image: 'loxam-access.png',
+		url: 'http://www.loxam-access.com/'
+	},
+	{
+		name: 'Nigloland',
+		image: 'nigloland.png',
+		url: 'https://www.nigloland.fr/'
+	},
+	{
+		name: 'NRJ',
+		image: 'nrj.png',
+		url: 'http://www.nrj.fr/'
+	},
+	{
+		name: 'Patrick Artifice',
+		image: 'patrick-artifice.png',
+		url: 'https://www.artifice-deco10.com/'
+	},
+	{
+		name: 'Premium Automobiles',
+		image: 'premium-automobiles.png',
+		url: 'https://premiumautomobiles-troyes.espacevo.fr/'
+	},
+	{
+		name: 'Rive Gauche',
+		image: 'rive-gauche.png',
+		url: 'https://www.facebook.com/rivegauchecafe/'
+	},
+	{
+		name: 'Société Générale',
+		image: 'societe-generale.png',
+		url: 'https://www.societegenerale.fr/'
+	},
+	{
+		name: 'SOCOTEC',
+		image: 'socotec.png',
+		url: 'http://www.socotec.com/fr/'
+	},
+	{
+		name: 'SoonNight',
+		image: 'soonnight.png',
+		url: 'https://www.soonnight.com/champagne-ardenne/'
+	},
+	{
+		name: 'Tekliss',
+		image: 'tekliss.png',
+		url: 'http://www.tekliss.com/'
+	},
+	{
+		name: 'UTT',
+		image: 'utt.png',
+		url: 'https://www.utt.fr/'
+	}
+];
