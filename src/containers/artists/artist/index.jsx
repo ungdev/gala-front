f0218c3c9d28c6ade1b1b5a0003e8ff0bd407bf3
Valentin Peltier <valentinpeltier@yahoo.fr>
2019-02-28 00:25:25 +0100

    Update artist component

--- src/containers/artists/artist/index.jsx
+++ src/containers/artists/artist/index.jsx
@@ -16,9 +16,25 @@ class Artist extends React.Component {
 					<div className="artist-infos">
 						<div className="artist-name">{this.props.name}</div>
 						{ this.props.description && <div className="artist-description">{this.props.description}</div> }
+
 						{ (this.props.hour || this.props.place) && <div className="artist-divider"></div> }
-						{ this.props.hour && <div className="artist-hour"><i className="far fa-clock icon"></i> {this.props.hour}</div> }
-						{ this.props.place && <div className="artist-place"><i className="fas fa-map-marker-alt icon"></i> {this.props.place}</div> }
+
+						{ this.props.hour && (
+							<div className="artist-hour">
+								<i className="far fa-clock icon"></i>
+								{this.props.hour}
+							</div>
+						)}
+						{ this.props.place && (
+							<div className="artist-place">
+								<i className="fas fa-map-marker-alt icon"></i>
+								{this.props.place}
+							</div>
+						)}
+
+						{ this.props.link && (
+							<a href={this.props.link} className="artist-link">Page de l'artiste <i class="fas fa-external-link-alt icon"></i></a>
+						)}
 					</div>
 				</div>
 			</div>
