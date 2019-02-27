c6ef34f50f6272bb1c6390688873d896c3ab1399
Valentin Peltier <valentinpeltier@yahoo.fr>
2019-02-27 21:09:52 +0100

    Add partners

--- src/variables/partners.js
+++ src/variables/partners.js
@@ -5,8 +5,68 @@ export default [
 		url: 'https://etu.utt.fr/orgas/bde'
 	},
 	{
+		name: 'Campus 3',
+		image: 'campus3.png',
+		url: 'https://campus3.fr/'
+	},
+	{
+		name: 'Festilight',
+		image: 'festilight.png',
+		url: 'https://www.festilight.com/'
+	},
+	{
+		name: 'ID Verde',
+		image: 'idverde.png',
+		url: 'https://idverde.com/'
+	},
+	{
+		name: 'Loxam Access',
+		image: 'loxam-access.png',
+		url: 'http://www.loxam-access.com/'
+	},
+	{
+		name: 'Nigloland',
+		image: 'nigloland.png',
+		url: 'https://www.nigloland.fr/'
+	},
+	{
+		name: 'NRJ',
+		image: 'nrj.png',
+		url: 'http://www.nrj.fr/'
+	},
+	{
+		name: 'Patrick Artifice',
+		image: 'patrick-artifice.png',
+		url: 'https://www.artifice-deco10.com/'
+	},
+	{
+		name: 'Premium Automobiles',
+		image: 'premium-automobiles.png',
+		url: 'https://premiumautomobiles-troyes.espacevo.fr/'
+	},
+	{
+		name: 'SOCOTEC',
+		image: 'socotec.png',
+		url: 'http://www.socotec.com/fr/'
+	},
+	{
+		name: 'Tekliss',
+		image: 'tekliss.png',
+		url: 'http://www.tekliss.com/'
+	},
+	{
 		name: 'UTT',
 		image: 'utt.png',
 		url: 'https://www.utt.fr/'
+	},
+	{
+		name: 'Moodyx',
+		image: 'moodyx.png',
+		url: 'https://moodyx.com/'
+	},
+	{
+		name: 'Troyes Habitat',
+		image: 'troyes_habitat.png',
+		url: 'https://www.troyes-habitat.com'
 	}
 ];
