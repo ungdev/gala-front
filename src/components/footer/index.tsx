54aa1000f9f0ddce9f048938020da5287f260dcb
Teddy Roncin <teddy.roncin@utt.fr>
2022-02-18 18:11:16 +0100

    update : footer content and style

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,32 +3,59 @@ import { Link } from 'react-router-dom';
 
 import './footer.scss';
 
+import cassiopee_icon from '../../assets/ic-cassiopee.png';
+
 export default () => (
   <footer>
-    <div className="store-links"></div>
+    <img className="logo" src={cassiopee_icon}></img>
 
-    <div className="social-links">
-      <a className="social-facebook" href="https://www.facebook.com/gala.utt">
-        <i className="fab fa-facebook-f" />
-      </a>
-      <a className="social-youtube" href="https://www.youtube.com/channel/UCLprjLc5CJMNUjSolrTyu4g">
-        <i className="fab fa-youtube" />
-      </a>
-      <Link className="social-contact" to="/contact">
-        <i className="far fa-envelope" />
-        <i className="far fa-envelope-open open" />
+    <div className="links">
+      <Link to="/about" className="animate">
+        A propos
+      </Link>
+      <br />
+      <Link to="/artists" className="animate">
+        Les artistes
+      </Link>
+      <br />
+      <Link to="/covid-19" className="animate">
+        Covid-19
+      </Link>
+      <br />
+      <Link to="/shop" className="animate">
+        Billeterie
       </Link>
     </div>
 
-    <small>
-      <div className="copyright">
-        <i className="far fa-copyright" />
-        Tous droits réservés - Gala UTT 2021
+    <div className="external-and-legal">
+      <div className="social-links">
+        <a className="social-facebook" href="https://www.facebook.com/gala.utt">
+          <i className="fab fa-facebook-f" />
+        </a>
+        <a className="social-youtube" href="https://www.youtube.com/channel/UCLprjLc5CJMNUjSolrTyu4g">
+          <i className="fab fa-youtube" />
+        </a>
+        <a className="social-instagram" href="https://www.instagram.com/cassiopee_galautt/">
+          <i className="fab fa-instagram" />
+        </a>
       </div>
 
-      <div className="links">
-        <Link to="/mentions-legales">Mentions légales</Link> - <Link to="/confidentialite">Confidentialité</Link>
-      </div>
-    </small>
+      <small className="legal">
+        <div className="copyright">
+          <i className="far fa-copyright" />
+          Tous droits réservés - Gala UTT 2022
+        </div>
+
+        <div className="legal-links">
+          <Link to="/mentions-legales" className="animate">
+            Mentions légales
+          </Link>{' '}
+          -{' '}
+          <Link to="/confidentialite" className="animate">
+            Confidentialité
+          </Link>
+        </div>
+      </small>
+    </div>
   </footer>
 );
