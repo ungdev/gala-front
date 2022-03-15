c2bfa920c8e76dfdb21d97963c74eae480dd76e2
AlbanSdl <alban.delavoreille@free.fr>
2022-03-15 20:48:37 +0100

    fix(home): proper hour on the landing page

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -36,7 +36,7 @@ function Home() {
     <div id="home">
       <div className="poster-container">
         <Countdown
-          date="14 May 2022 21:00:00 UTC+0200"
+          date="14 May 2022 20:00:00 UTC+0200"
           renderer={(props) => {
             if (!props.days && !props.hours && !props.minutes && !props.seconds) {
               return null;
